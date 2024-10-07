989ab40fdbd40334008841ade949ac09ea51b777
Sevensiyush-hembrom-au51 <stevensiyush0777@gmail.com>
2024-10-07 12:46:23 +0530

    .

---
+++ MERN APP/my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est facere
+          sequi neque ipsam quae facilis quo quisquam! Eius minus itaque sed!
+          Repudiandae nostrum illum velit quo aliquid atque, eveniet dicta!
+          Doloribus, non. Autem, consequatur!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
